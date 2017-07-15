3966c25099eb81fbdf86b483a674d4a421b597e3
ssahib <shivankaul.1993@gmail.com>
2017-07-15 13:10:31 -0400

    Restrict badge animation to tab
    Code needs clean up

--- src/background.js
+++ src/background.js
@@ -1,27 +1,56 @@
+let blockedResources = {};
+
 chrome.webRequest.onCompleted.addListener(function(details) {
     if (details.statusCode == 451) {
-        notify(details);
-        setBrowserAction({ text: '451', color: 'red' });
-        setTimeout(function() {
-            setBrowserAction({ clear: true });
-        }, 3000);
+        getCurrentTabId(details, addToBlocked);
     }
 }, { urls: ["<all_urls>"] });
+
+function addToBlocked(tabId, url, details) {
+    if (blockedResources[url]) {
+        blockedResources[url].push(details.url);
+    } else {
+        blockedResources[url] = [details.url];
+    }
+    console.log(blockedResources);
+    chrome.browserAction.setBadgeText({ text: '451', tabId: tabId });
+    chrome.browserAction.setBadgeBackgroundColor({ color: 'red', tabId: tabId });
+    notify(details);
+}
 
 /* 
  * params -> [[clear (boolean) | text (string)] | color (string)]
  */
 function setBrowserAction(params) {
     if (params['text']) {
-        chrome.browserAction.setBadgeText({ text: params['text'] });
+        chrome.browserAction.setBadgeText({ text: params['text'], tabId: params['tabId'] });
     } else if (params['clear']) {
         chrome.browserAction.setBadgeText({ text: '' });
     }
     if (params['color']) {
         chrome.browserAction
-            .setBadgeBackgroundColor({ color: params['color'] });
+            .setBadgeBackgroundColor({ color: params['color'], tabId: params['tabId']  });
     }
 }
+
+/**
+ * Get the current URL. Citation: https://developer.chrome.com/extensions/getstarted
+ * @param {function(string)} callback - called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabId(details, callback) {
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+    chrome.tabs.query(queryInfo, function(tabs) {
+        var tab = tabs[0];
+        var url = tab.url;
+        var tabId = tab.id;
+        console.log("tab id is " + tabId)
+        callback(tabId, url, details);
+    })
+};
 
 function notify(details) {
     const resourceUrl = details.url;
